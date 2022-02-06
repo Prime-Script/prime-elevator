d4673bebf62dc180333a2695a2330f4f20f767da
NocityRP <93712286+NocityRP@users.noreply.github.com>
2022-02-06 00:36:15 +0000

    missed fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SocialPeely#4095'
+description 'Lift System For QB-Core'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_script 'client/*.lua'
+
+dependency 'qb-target'
+
+lua54 'yes'
